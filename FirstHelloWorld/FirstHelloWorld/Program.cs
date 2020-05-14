32fad2a274d081f3e13aa1750b0f96be1ee0402b
sangTran <sangthien2010@gmail.com>
2020-05-13 20:45:40 -0500

    add first project

---
+++ FirstHelloWorld/FirstHelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+        }
+    }
+}
