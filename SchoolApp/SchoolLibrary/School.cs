5ea839236271547c5cc9ce5247e6255873e23a7d
sangTran <sangthien2010@gmail.com>
2020-05-15 11:19:30 -0500

    adding constructor

--- SchoolApp/SchoolLibrary/School.cs
+++ SchoolApp/SchoolLibrary/School.cs
@@ -12,7 +12,7 @@ namespace SchoolLibrary
         public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
-        public string Number { get; set; }
+        public string PhoneNumber { get; set; }
 
         private string _twitterAddress;
         public string TwitterAddress
@@ -33,5 +33,18 @@ namespace SchoolLibrary
 
         Elementary.Volume volume;
         HighSchool.Volume volume2;
+
+        //ctor and tab 2 times for constructor generator
+        public School()
+        {
+            Name = "Untitled School";
+            PhoneNumber = "555-1234";
+        }
+
+        public School(string SchoolName, string SchoolNumber)
+        {
+            Name = SchoolName;
+            PhoneNumber = SchoolNumber;
+        }
     }
 }
