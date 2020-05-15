7608ca69ec742c23df0436003b75cdb8c2a0c838
sangTran <sangthien2010@gmail.com>
2020-05-15 11:38:19 -0500

    add override toString method

--- SchoolApp/SchoolLibrary/School.cs
+++ SchoolApp/SchoolLibrary/School.cs
@@ -53,13 +53,27 @@ namespace SchoolLibrary
         //    return result;
         //}
 
-        public float AverageThreeScores(float a, float b, float c) => (a + b + c) / 3;
+        public static float AverageThreeScores(float a, float b, float c) => (a + b + c) / 3;
 
-        public int AverageThreeScores(int a, int b, int c)
+        public static int AverageThreeScores(int a, int b, int c)
         {
             var result = (a + b + c) / 3;
             return result;
 
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(Name);
+            sb.AppendLine(Address);
+            sb.AppendLine(this.City);
+            sb.Append(" ,");
+            sb.Append(this.State);
+            sb.Append(" ");
+            sb.Append(Zip);
+
+            return sb.ToString();
         }
     }
 }
