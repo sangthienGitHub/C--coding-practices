44fcb8fc75bf966cc77082f9595b1d7607454634
sangTran <sangthien2010@gmail.com>
2020-05-15 10:01:28 -0500

    add new folder and auto-properties

---
+++ SchoolApp/SchoolLibrary/School.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        //Auto Properties
+        string Name { get; set; }
+        string Address { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string Zip { get; set; }
+        string Number { get; set; }
+
+        Elementary.Volume volume;
+        HighSchool.Volume volume2;
+    }
+}
