467f093db90c2e0305c5452b5b12862e8d1e32ee
sangTran <sangthien2010@gmail.com>
2020-05-16 14:09:19 -0500

    extension methods

--- SchoolApp/SchoolLibrary/EnglishPaper.cs
+++ SchoolApp/SchoolLibrary/EnglishPaper.cs
@@ -10,5 +10,9 @@ namespace SchoolLibrary
         public string Title { get; set; }
         public int MinimumWordCount { get; set; }
         public string PaperText { get; set; }
+        public int WordCount
+        {
+            get { return PaperText.WordCount(); }
+        }
     }
 }
