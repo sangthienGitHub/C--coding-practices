a23d8dee707eab6c8fca36a6b09bb36f9debbfeb
sangTran <sangthien2010@gmail.com>
2020-05-16 13:52:35 -0500

    create a new interface and insert it into class

---
+++ SchoolApp/SchoolLibrary/EnglishPaper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class EnglishPaper
+    {
+        public string Title { get; set; }
+        public int MinimumWordCount { get; set; }
+        public string PaperText { get; set; }
+    }
+}
